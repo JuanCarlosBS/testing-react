4245c5415c5e429fc25c7a56af10e5f3d51e1aeb
juancarlosbs <juanrov_@hotmail.com>
2021-05-11 14:53:11 -0300

    test: writing tests using interaction & state

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from 'react'
+
 export default function Greeting() {
+    const [changedText, setChangedText] = useState(false)
+
+    const changeTextHandler = () => {
+        setChangedText(true)
+    }
+
     return (
         <div>
-            <h2>Hello World</h2>
-            <p>It's good to see you!</p>
+            <h2>Hello World!</h2>
+            {!changedText && <p>It's good to see you!</p>} 
+            {changedText && <p>Changed!</p>} 
+            <button onClick={changeTextHandler}>Changed Text</button>
         </div>
     )
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,9 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting'
 
-test('renders Hello World as a text', () => {
-    render(<Greeting />)
+describe('Geting component', () => {
+    test('renders Hello World as a text', () => {
+        render(<Greeting />)
+    
+        const helloWorldElement = screen.getByText('Hello World!')
+        expect(helloWorldElement).toBeInTheDocument()
+    }) 
 
-    const helloWorldElement = screen.getByText('Hello World')
-    expect(helloWorldElement).toBeInTheDocument()
-}) 
+    test('renders good to src you if the button was NOT clicked', () => {
+        render(<Greeting />)
+
+
+    })
+
+    test('renders Changed! if the button was clicked', () => {
+        render(<Greeting />)
+
+        const buttonElement = screen.getByRole('button')
+        userEvent.click(buttonElement)
+
+        const outputElement = screen.getByText('Changed!')
+        expect(outputElement).toBeInTheDocument();
+    })
+})
+
